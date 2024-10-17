55f50a1a5603626841c847367db529f6bf1090e1
JhonatanEfrain <jhonatan.valdivia99@gmail.com>
2024-10-17 18:41:13 -0500

    Añadir Model Usuario

---
+++ TresLagunas_AppWeb/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace TresLagunas_AppWeb.Models
+{
+    public class Usuario
+    {
+        public int IDUsuario { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Correo {  get; set; }
+        public string contraseña { get; set; }
+
+    }
+}
