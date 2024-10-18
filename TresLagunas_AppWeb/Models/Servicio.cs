976a30aa6d39e70b3b4618b6828d06cb0b29f892
JhonatanEfrain <jhonatan.valdivia99@gmail.com>
2024-10-18 15:23:46 -0500

    Modelos faltantes

---
+++ TresLagunas_AppWeb/Models/Servicio.cs
@@ -0,0 +1,10 @@
+﻿namespace TresLagunas_AppWeb.Models
+{
+    public class Servicio
+    {
+        public int IDServicio {  get; set; }
+        public string NombreServicio { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
---
+++ TresLagunas_AppWeb/Models/Solicitud.cs
@@ -0,0 +1,12 @@
+﻿namespace TresLagunas_AppWeb.Models
+{
+    public class Solicitud
+    {
+        public int IDSolicitud {  get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos {  get; set; }
+        public string Telefono { get; set; }
+        public string Correo {  get; set; }
+        public string Descripcion { get; set; }
+    }
+}
