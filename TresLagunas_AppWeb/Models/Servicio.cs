f3ebc0944dfbc5e4cf7c62d0858ac621e92214fb
Mireli <gvasquezd22_2@unc.edu.pe>
2024-10-18 15:52:03 -0500

    Restricciones en servicio y usuario

--- TresLagunas_AppWeb/Models/Servicio.cs
+++ TresLagunas_AppWeb/Models/Servicio.cs
@@ -1,9 +1,15 @@
-﻿namespace TresLagunas_AppWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TresLagunas_AppWeb.Models
 {
     public class Servicio
     {
-        public int IDServicio {  get; set; }
+        public int IDServicio { get; set; }
+        [Display(Name = "Nombre de servicio")]
+        [Required(ErrorMessage = "Campo nombre de servicio obligatorio")]
         public string NombreServicio { get; set; }
+        [Display(Name = "Descripción")]
+        [Required(ErrorMessage = "Campo descripción de servicio obligatorio")]
         public string Descripcion { get; set; }
         public ICollection<Solicitud> Solicitudes { get; set; }
     }
--- TresLagunas_AppWeb/Models/Usuario.cs
+++ TresLagunas_AppWeb/Models/Usuario.cs
@@ -1,12 +1,21 @@
-﻿namespace TresLagunas_AppWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TresLagunas_AppWeb.Models
 {
     public class Usuario
     {
         public int IDUsuario { get; set; }
+        [Required(ErrorMessage = "Campo nombres obligatorio")]
         public string Nombres { get; set; }
+        [Required(ErrorMessage = "Campo apellidos obligatorio")]
         public string Apellidos { get; set; }
+        [Display(Name = "Correo electrónico")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Campo correo electrónico obligatorio")]
         public string Correo {  get; set; }
-        public string contraseña { get; set; }
+        [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "Campo contraseña obligatorio")]
+        public string contrasena { get; set; }
 
     }
 }
